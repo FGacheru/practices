df60049af8d31bfa8baa4cc4cf5910bca1764ffc
FGacheru <gacheruf12@gmail.com>
2023-08-14 10:29:32 +0300

    Learned @Output() and the event emitter

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -4,29 +4,27 @@ import { PostListComponent } from './post-list/post-list.component';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
-export class PostComponent implements AfterViewInit{
-
+export class PostComponent implements AfterViewInit {
   fromPost: string = 'This message is from the post component';
-
+  receivedMessage: string = '';
   message: string = '';
   @Input() fromParent: string = '';
 
-  post: string = "Second message from post";
+  post: string = 'Second message from post';
 
   @ViewChild(PostListComponent) childComp?: PostListComponent;
 
-  constructor() {
-
-  }
-
   ngAfterViewInit() {
-    if(this.childComp){
-      this.message = this.childComp!.forParent
+    if (this.childComp) {
+      this.message = this.childComp!.forParent;
     } else {
-      'Empty'
+      ('Empty');
     }
   }
 
+  parentGreet(name: string) {
+    alert("Hello " + name);
+  }
 }
