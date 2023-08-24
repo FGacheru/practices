b5b9354b62ac017ef63b07b136a0a5ffb4fd80e8
FGacheru <gacheruf12@gmail.com>
2023-08-24 12:51:55 +0300

    Create a delete function with the ngFor Directive, it's very easy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,11 @@ export class AppComponent implements AfterViewInit {
   postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4']
   objArray: Array<any> = [
     {id:1, postTitle: 'Post 1'},
-    {id:1, postTitle: 'Post 1'},
-    {id:1, postTitle: 'Post 1'},
-    {id:1, postTitle: 'Post 1'},
+    {id:2, postTitle: 'Post 2'},
+    {id:3, postTitle: 'Post 3'},
+    {id:4, postTitle: 'Post 4'},
+    {id:5, postTitle: 'Post 5'},
+
 
   ]
   @ViewChild(PostComponent) postComp!: PostComponent;
@@ -45,5 +47,13 @@ export class AppComponent implements AfterViewInit {
       console.log(this.postArray.at(i));
       
     }
+  }
+
+  addNew(){
+    this.objArray.push({id: 6, postTitle: 'Post 6'})
+  }
+
+  onDelete(index: any){
+    this.objArray.splice(index, 1);
   }
 }
